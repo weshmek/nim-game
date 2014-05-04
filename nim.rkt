5fdb58c3fb10e97d1b30fe9b648251fcb4c322dd
weshmek <weshmek@github.com>
2014-05-04 17:06:52 -0400

    Tried to make the code prettier

--- nim.rkt
+++ nim.rkt
@@ -30,41 +30,35 @@
 (define nim-sum bitwise-xor)
 
 
-(define dumb-move
-  (lambda nim-board
+(define dumb-move-as-move
+  (lambda (row . nim-board)
     (cond
       [(empty? nim-board) empty]
-      [(zero? (first nim-board)) (cons (first nim-board) (apply dumb-move (rest nim-board)))]
-      [else (cons (- (first nim-board) 1) (rest nim-board))])))
+      [(zero? (first nim-board)) (apply dumb-move-as-move (+ row 1) (rest nim-board))]
+      [else (values row 1)])))
 
 
-(dumb-move 1 2 3 4 5)
+(dumb-move-as-move 1 2 3 4 5)
 
 
-;;best-move nim-board -> nim-board
 
-(define best-move
+(define best-move-as-move
   (lambda nim-board
-    (define B nim-board)
     (define X (apply nim-sum nim-board))
-    (cond 
-      [(zero? X) (apply dumb-move nim-board)]
+    (cond
+      [(zero? X) (apply dumb-move-as-move 1 nim-board)]
       [else
-       (define helper
-         (lambda nim-board
-           (cond
-             [(empty? nim-board) empty]
-             [else (let ([Y (nim-sum X (first nim-board))])
-                     (cond 
-                       [ (<= Y (first nim-board)) (cons Y (rest nim-board))]
-                       [else (cons (first nim-board) (apply helper (rest nim-board)))]))])))
-       (apply helper nim-board)])))
+       (define (helper row . nim-board)
+         (cond
+           [(empty? nim-board) empty]
+           [else (let ([Y (nim-sum X (first nim-board))])
+                   (cond
+                     [(<= Y (first nim-board)) (values row (- (first nim-board) Y))]
+                     [else (apply helper (+ row 1) (rest nim-board))]))]))
+       
+       (apply helper 1 nim-board)])))
 
-
-(best-move 9 6 5 4 3)
-(best-move 7 6 5 4 3)
-
-
+(best-move-as-move 9 6 5 4 3)
 
 
 
@@ -73,10 +67,10 @@
     (foldr (lambda (x y) (and x y)) #t (map zero? nim-board))))
 
 
-(define (user-move row num-take . nim-board)
+(define (move row num-take . nim-board)
   (cond
     [(empty? nim-board) (error "invalid row!")]
-    [((compose not =) row 1) (cons (first nim-board) (apply user-move (- row 1) num-take (rest nim-board)))]
+    [((compose not =) row 1) (cons (first nim-board) (apply move (- row 1) num-take (rest nim-board)))]
     [(< (first nim-board) num-take) (error "invalid number to take!")]
     [else (cons (- (first nim-board) num-take) (rest nim-board))]))
 
@@ -90,29 +84,36 @@
     ['computer (computer-fn)]))
 
 
+(define (get-user-move)
+  (let*
+      ([row (begin
+              (printf "Type the row to take from:")
+              (read))]
+       [num-take
+        (begin
+          (printf "~nType the number of items to take from row ~a: " row)
+          (read))])
+    (printf "~n")
+    (values row num-take)))
+
 
 (define (play-nim turn . nim-board)
-    (apply nim-board-print nim-board)
-    (let ([B 
-           (turn-choice turn (lambda () (let*
-                                            ([row (begin (printf "Type the row to take from: ")
-                                                  (read))]
-                                             [num-take (begin (printf "Type the number of items to take from row ~a: " row)
-                                                       (read))])
-                                          (printf "~n")
-                                          (apply user-move row num-take nim-board)))
-                        (lambda () (apply best-move nim-board)))])
-      
-      (cond
-        [(apply did-win? B)
-         (apply nim-board-print nim-board)
-         (turn-choice turn (lambda () (printf "Congratulations! You won!~n")) (lambda () (printf "Looks like I won!")))]
-        [else
-         (apply play-nim
-                (turn-choice turn (lambda () 'computer) (lambda () 'user))
-                B)])))
-                       
-                       
+  (apply nim-board-print nim-board)
+  (define-values (row num-take) (turn-choice turn
+                                             get-user-move
+                                             (thunk (apply best-move-as-move nim-board))))
+  (define B (apply move row num-take nim-board))
+  
+  (cond
+    [(apply did-win? B)
+     (apply nim-board-print nim-board)
+     (turn-choice turn (thunk (printf "Congratulations! You won!~n")) (thunk (printf "Looks like I won!~n")))]
+    [else
+     (apply play-nim
+            (turn-choice turn (thunk 'computer) (thunk 'user))
+            B)]))
+
+
 
 
 
@@ -153,3 +154,28 @@
 ;             [else (apply play-nim B)]))]))))
 ;
 ;
+
+;;;best-move nim-board -> nim-board
+;
+;(define best-move
+;  (lambda nim-board
+;    (define X (apply nim-sum nim-board))
+;    (cond 
+;      [(zero? X) (apply dumb-move nim-board)]
+;      [else
+;       (define helper
+;         (lambda nim-board
+;           (cond
+;             [(empty? nim-board) empty]
+;             [else (let ([Y (nim-sum X (first nim-board))])
+;                     (cond 
+;                       [ (<= Y (first nim-board)) (cons Y (rest nim-board))]
+;                       [else (cons (first nim-board) (apply helper (rest nim-board)))]))])))
+;       (apply helper nim-board)])))
+;
+;
+;
+;
+;(best-move 9 6 5 4 3)
+;(best-move 7 6 5 4 3)
+
